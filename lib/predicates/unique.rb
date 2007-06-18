5d2277076710bbe66eb3409605e07e1233439cca
cainlevy <cainlevy@253fb8bd-cf2e-0410-89f4-458c444950b4>
2007-06-18 22:55:41 +0000

    adding Unique
    git-svn-id: http://semanticattributes.googlecode.com/svn/trunk@19 253fb8bd-cf2e-0410-89f4-458c444950b4

---
+++ lib/predicates/unique.rb
@@ -0,0 +1,45 @@
+# Describes an attribute as being unique, possibly within a certain scope.
+# options:
+# * :scope => [...] - a list of other fields that define the context for uniqueness. it's like defining a multi-column uniqueness constraint.
+# * :case_sensitive => boolean - whether case matters for uniqueness.
+class Predicates::Unique < Predicates::Enumerated
+  attr_accessor :case_sensitive
+  attr_accessor :scope
+  
+  def initialize(attribute, options = {})
+    defaults = {
+      :scope => [],
+      :case_sensitive => true
+    }
+    super attribute, defaults.merge(options)
+  end
+
+  def error_message
+    @error_message || "%s has already been taken."
+  end
+
+  def validate(value, record)
+    conditions_hash = {@attribute => value}
+    self.scope.each { |attribute| conditions_hash[attribute] = record.send(attribute) }
+    
+    conditions_array = ['']
+    conditions_hash.each_pair do |attribute, attribute_value|
+      sql = "#{record.class.table_name}.#{attribute}"
+      unless self.case_sensitive
+        sql = "LOWER(#{sql})"
+        attribute_value.downcase!
+      end
+      sql << ' ' << record.class.send(:attribute_condition, attribute_value)
+      
+      conditions_array.first << ' AND ' unless conditions_array.first.empty?
+      conditions_array.first << sql
+      conditions_array << attribute_value
+    end
+
+    result = record.class.find(:all, :conditions => conditions_array, :limit => 2)
+    return (result.size < 1 or result.first == record)
+  end
+
+  def to_human(v); v; end
+  def from_human(v); v; end
+end
