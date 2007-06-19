5aebb109ee81874a8aab5cbb0e6dc1c26b2c6fc8
cainlevy <cainlevy@253fb8bd-cf2e-0410-89f4-458c444950b4>
2007-06-19 20:47:42 +0000

    adding some compatibility methods for creating semantic attributes with common attachment plugins (file_columns, acts_as_attachment, attachment_fu) and the enum-column-for-rails plugin.
    git-svn-id: http://semanticattributes.googlecode.com/svn/trunk@21 253fb8bd-cf2e-0410-89f4-458c444950b4

---
+++ lib/compatibility.rb
@@ -0,0 +1,42 @@
+module SemanticAttributes
+  # Integrates other common plugins and core Rails methods with SemanticAttributes
+  # This module is not automatically mixed-in for two reasons:
+  # 1) it may not be desired
+  # 2) it needs to happen *after* all other plugins have been initialized
+  module Compatibility
+    def self.included(base)
+      base.extend ClassMethods
+    end
+    
+    module ClassMethods
+      def self.included(base)
+        # enum-column-for-rails
+        # http://rubyforge.org/projects/enum-column/
+        columns.collect {|c| c.type == :enum}.each do |enum_column|
+          self.semantic_attributes[attr].add 'enumeration', :options => enum_column.values
+        end
+      end
+    
+      # file_column
+      # http://www.kanthak.net/opensource/file_column/
+      def file_column(attr, *args)
+        self.semantic_attributes[attr].add 'file'
+        super
+      end
+      
+      # acts_as_attachment
+      # http://technoweenie.stikipad.com/plugins/show/Acts+as+Attachment
+      def acts_as_attachment(*args)
+        self.semantic_attributes[:uploaded_data].add 'file'
+        super
+      end
+      
+      # attachment_fu
+      # http://weblog.techno-weenie.net/2007/2/25/attachment_fu-tutorial
+      def has_attachment
+        self.semantic_attributes[:uploaded_data].add 'file'
+        super
+      end
+    end
+  end
+end
