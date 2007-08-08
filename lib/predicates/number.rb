401e56cceb146ae7ad114134da37a76ab10333d7
cainlevy <cainlevy@253fb8bd-cf2e-0410-89f4-458c444950b4>
2007-08-08 00:20:22 +0000

    merging the Range predicate into the Number predicate. really, it was just an extra set of options related to numbers, and a "range" is a funny predicate. hopefully i can refine the others more as well.
    git-svn-id: http://semanticattributes.googlecode.com/svn/trunk@36 253fb8bd-cf2e-0410-89f4-458c444950b4

--- lib/predicates/number.rb
+++ lib/predicates/number.rb
@@ -1,25 +1,84 @@
-# Describes an attribute as a number.
+# Describes an attribute as a number. You may specify boundaries for the number (min, max, or both), and also specify whether it must be an integer.
 #
-# ==Example
+# ==Options
+# * :integer [boolean, default: false] - if the number is an integer (or float/decimal)
+# * :above [integer, float] - when the number has a minimum
+# * :below [integer, float] - when the number has a maximum
+# * :range [range] - when the number has a minimum and a maximum
+# * :inclusive [boolean, default: false] - if your maximum or minimum is also an allowed value. Does not work with :range.
+#
+# ==Examples
 #   field_is_a_number :integer => true
+#   field_is_a_number :range => 1..5, :integer => true
+#   field_is_a_number :above => 4.5
+#   field_is_a_number :below => 4.5, :inclusive => true
 class Predicates::Number < Predicates::Base
   # whether to require an integer value
   attr_accessor :integer
 
+  # when the number has a minimum value, but no maximum
+  attr_accessor :above
+
+  # when the number has a maximum value, but no minimum
+  attr_accessor :below
+
+  # when the number has both a maximum and a minimum value
+  attr_accessor :range
+
+  # meant to be used with :above and :below, when you want the endpoint to be inclusive.
+  # with the :range option you can just specify inclusion using the standard Ruby range syntax.
+  attr_accessor :inclusive
+
   def error_message
-    @error_message || " must be a number."
+    @error_message || "must be a number#{range_description}."
   end
 
   def validate(value, record)
-    if integer
-      value.to_s.match(/\A[+-]?[0-9]+\Z/) unless value.is_a? Hash or value.is_a? Array
-    else
-      begin
-        Kernel.Float(value)
-        true
-      rescue ArgumentError, TypeError
-        false
+
+    # check data type
+    valid = if self.integer
+        # if it must be an integer, do a regexp check for digits only
+        value.to_s.match(/\A[+-]?[0-9]+\Z/) unless value.is_a? Hash or value.is_a? Array
+      else
+        # if it can also be a float or decimal, then try a conversion
+        begin
+          Kernel.Float(value)
+          true
+        rescue ArgumentError, TypeError
+          false
+        end
       end
+
+    # if it's the right data type, then also check boundaries
+    valid &&= if self.range
+        self.range.include? value
+      elsif self.above
+        operator = self.inclusive ? :>= : :>
+        value.send operator, self.above
+      elsif self.below
+        operator = self.inclusive ? :<= : :<
+        value.send operator, self.below
+      else
+        true
+      end
+
+    valid
+  end
+
+  protected
+
+  def range_description
+    # if it has two endpoints
+    return " from #{self.range.first} #{self.range.exclude_end? ? 'to' : 'through'} #{self.range.last}" if self.range
+    # if it only has one inclusive endpoint
+    if inclusive
+      return " at least #{self.above}" if self.above
+      return " no more than #{self.below}" if self.below
+    # if it only has one exclusive endpoint
+    else
+      return " greater than #{self.above}" if self.above
+      return " less than #{self.below}" if self.below
     end
+    # if it has no endpoints
   end
 end
--- test/predicates/number_test.rb
+++ test/predicates/number_test.rb
@@ -5,6 +5,12 @@ class NumberPredicateTest < Test::Unit::TestCase
     @predicate = Predicates::Number.new(:foo)
   end
 
+  def test_basic_error_message
+    assert_equal 'must be a number.', @predicate.error_message
+    @predicate.error_message = 'foo'
+    assert_equal 'foo', @predicate.error_message
+  end
+
   def test_integers
     @predicate.integer = true
 
@@ -30,9 +36,74 @@ class NumberPredicateTest < Test::Unit::TestCase
     assert @predicate.validate(5.2e-5, nil)
   end
 
-  def test_error_message
-    assert_equal ' must be a number.', @predicate.error_message
-    @predicate.error_message = 'foo'
-    assert_equal 'foo', @predicate.error_message
+  def test_min
+    @predicate.above = 5
+    assert_equal 'must be a number greater than 5.', @predicate.error_message
+
+    assert !@predicate.validate(-10, nil)
+    assert !@predicate.validate(-5, nil)
+    assert !@predicate.validate(0, nil)
+    assert !@predicate.validate(5, nil)
+    assert @predicate.validate(10, nil)
+
+    assert @predicate.validate(5.0001, nil)
+    @predicate.integer = true
+    assert !@predicate.validate(5.0001, nil)
+  end
+
+  def test_min_inclusive
+    @predicate.above = 5
+    @predicate.inclusive = true
+    assert_equal 'must be a number at least 5.', @predicate.error_message
+
+    assert !@predicate.validate(4, nil)
+    assert @predicate.validate(5, nil)
+  end
+
+  def test_max
+    @predicate.below = 5
+    assert_equal 'must be a number less than 5.', @predicate.error_message
+
+    assert @predicate.validate(-10, nil)
+    assert @predicate.validate(-5, nil)
+    assert @predicate.validate(0, nil)
+    assert !@predicate.validate(5, nil)
+    assert !@predicate.validate(10, nil)
+
+    assert @predicate.validate(4.9999, nil)
+    @predicate.integer = true
+    assert !@predicate.validate(4.9999, nil)
+  end
+
+  def test_max_inclusive
+    @predicate.below = 5
+    @predicate.inclusive = true
+    assert_equal 'must be a number no more than 5.', @predicate.error_message
+
+    assert @predicate.validate(5, nil)
+    assert !@predicate.validate(6, nil)
+  end
+
+  def test_range
+    @predicate.range = -5...5
+    assert_equal 'must be a number from -5 to 5.', @predicate.error_message
+
+    assert !@predicate.validate(-10, nil)
+    assert @predicate.validate(-5, nil)
+    assert @predicate.validate(0, nil)
+    assert !@predicate.validate(5, nil)
+    assert !@predicate.validate(10, nil)
+
+    assert @predicate.validate(4.9999, nil)
+    @predicate.integer = true
+    assert !@predicate.validate(4.9999, nil)
+  end
+
+  def test_range_inclusive
+    @predicate.range = -5..5
+    assert_equal 'must be a number from -5 through 5.', @predicate.error_message
+
+    assert @predicate.validate(5, nil)
+    assert !@predicate.validate(6, nil)
   end
 end
